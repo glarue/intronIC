517d44c050631ed458a022d770676c32201eb114
Graham Larue <egrahamlarue@gmail.com>
2021-07-30 16:02:35 -0700

    intronIC v1.3.1

--- intronIC/intronIC.py
+++ intronIC/intronIC.py
@@ -30,7 +30,7 @@ __author__ = 'Graham E. Larue'
 __maintainer__ = "Graham E. Larue"
 __email__ = 'egrahamlarue@gmail.com'
 __license__ = 'GPL v3.0'
-__version__ = '1.3.0'
+__version__ = '1.3.1'
 
 # imports
 import argparse
